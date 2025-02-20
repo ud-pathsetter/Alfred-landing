a8b1a94944d0c36d977236f066b2109601942571
uambwani3 <ambwaniudayan01@gmail.com>
2025-02-20 12:50:35 +0530

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import Script from "next/script";
 import { Toaster } from "react-hot-toast";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
@@ -23,6 +24,19 @@ export default function RootLayout({
     <html lang='en' className='relative'>
       <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         {children}
+        {/* Google tag (gtag.js) */}
+        <Script
+          src='https://www.googletagmanager.com/gtag/js?id=G-XX9JB0F56E'
+          strategy='afterInteractive'
+        />
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XX9JB0F56E');
+          `}
+        </Script>
         <Toaster position='top-right' reverseOrder={false} />
       </body>
     </html>
